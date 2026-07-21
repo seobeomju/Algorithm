87e2ded7b6d0ce8f69adb7819bf0f1154825d584
seobeomju <88228101+seobeomju@users.noreply.github.com>
2026-07-21 17:31:16 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 47.82 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,65 +1,40 @@
 import java.util.*;
 
-class Node{
-    int amount;
-    String enroll;
-    Node referral;
-    
-    Node(String enroll){
-        this.enroll = enroll;
-    }
-}
-
 class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
         int[] answer = new int[enroll.length];
+        HashMap<String,String> person = new HashMap<>();
+        HashMap<String,Integer> moneyMap = new HashMap<>();
         
-        //모든 사람 노드 만들기
-        HashMap<String,Node> map = new HashMap<>();
-        for(int i =0; i<enroll.length; i++){
-            // 생성자가 enroll이므로 ""로 생성말고 enroll로 생성
-            map.put(enroll[i],new Node(enroll[i]));
-        }
-        Node root = new Node("-");
-        //부모 연결
-        for(int i =0; i<referral.length;i++){
-            if(referral[i].equals("-")){
-                
-                map.get(enroll[i]).referral = root;
-            }else{
-            //System.out.println(map.get(enroll[i]));
-            map.get(enroll[i]).referral = map.get(referral[i]);
-            }
+        //판매자와 소개한 사람 map 생성
+        for(int i=0; i<enroll.length; i++){
+            person.put(enroll[i], referral[i]);
+            // moneyMap.put(enroll[i],0);
+            //System.out.println(person);
         }
         
         //판매 발생
         for(int i =0; i<seller.length; i++){
-            int money = amount[i] * 100;
-           
-            Node cur = map.get(seller[i]);
-            //System.out.println(cur);
-            //while(cur != root) 이것도 가능
-            while(!cur.enroll.equals("-")){
-                
+            int money = amount[i]*100;
+            String sellerName = seller[i];
+            
+            while(!sellerName.equals("-")){
                 int referralMoney = money/10;
-                
-                //한 사람이 두번 판매할 경우 대비
-                //map.get(seller[i]).amount = amount[i] * 100;
-                cur.amount += money - referralMoney ;
+                moneyMap.put(sellerName, moneyMap.getOrDefault(sellerName,0) + money-referralMoney);
                 
                 if(referralMoney == 0){
                     break;
                 }
                 
                 money = referralMoney;
-                cur = cur.referral;
-            } 
+                sellerName = person.get(sellerName);
+            }
+            
         }
         
-        for(int i=0; i<enroll.length;i++){
-            answer[i] = map.get(enroll[i]).amount;
+        for(int i=0; i<enroll.length; i++){
+            answer[i] = moneyMap.getOrDefault(enroll[i],0);
         }
-        
         
         
         
