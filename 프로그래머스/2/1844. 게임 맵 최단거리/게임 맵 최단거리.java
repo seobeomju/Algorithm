dbe8564ccc5ff66cc485ad47d8a80fdd0a7e6357
seobeomju <88228101+seobeomju@users.noreply.github.com>
2026-07-24 23:38:29 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.90 ms, Memory: 57.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,42 +2,44 @@ import java.util.*;
 class Solution {
     private static int[] dx = new int[]{1,-1,0,0};
     private static int[] dy = new int[]{0,0,-1,1};
+    
+    private static int[][] dist;
+    
     public int solution(int[][] maps) {
         int answer = 0;
         int n = maps.length;
-        int m = maps[0].length;
-
+        int m =maps[0].length;
+        dist = new int[n][m];
         int[] start = new int[]{0,0};
-        answer = bfs(start,maps,n,m);
-            
+        
+        answer = bfs(maps, start,n,m);
+        
         return answer;
     }
     
-    private static int bfs(int[] start, int[][] maps,int n,int m){
-        ArrayDeque<int[]> queue = new ArrayDeque<>();
-        //거리 겸 방문 여부 저장 배열
-        int[][] dist = new int[n][m];
+    private static int bfs(int[][] maps, int[] start,int n, int m){
+        ArrayDeque<int[]> q = new ArrayDeque<>();
         
-        queue.add(start);
-        dist[0][0] = 1;
+        q.add(start);
+        dist[start[0]][start[1]] = 1;
         
-        while(!queue.isEmpty()){
+        while(!q.isEmpty()){
+            int[] now =q.poll();
             
-            int[] now = queue.poll();
             
             for(int i=0; i<4; i++){
                 int nx = now[0] + dx[i];
                 int ny = now[1] + dy[i];
                 
-                if( 0<=nx && nx<n &&  0<=ny && ny<m){
-                   if(maps[nx][ny] == 1 && dist[nx][ny] == 0){
-                        dist[nx][ny] = dist[now[0]][now[1]] + 1;
-                        queue.add(new int[]{nx,ny});
-                   }
+                if( 0<=nx && nx <n && 0<=ny && ny<m ){
+                    if(maps[nx][ny] == 1 && dist[nx][ny] == 0){
+                        q.add(new int[]{nx,ny});
+                        dist[nx][ny] = dist[now[0]][now[1]]+ 1;
+                    }    
                 }
-            }     
-            
+            }
         }
+        
         return dist[n-1][m-1] == 0 ? -1 : dist[n-1][m-1];
     }
 }
