9a0d6f62df50e5f593252eba92257ffc33a0217c
seobeomju <88228101+seobeomju@users.noreply.github.com>
2026-07-24 21:46:27 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 56.70 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,22 +2,25 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        HashMap<String,Integer> map = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>();
+        
         for(String name : participant){
-            // getOrDefault 해당하는 키가 있으면 값을 가져오고 없으면 내가 지정하는 값을 저장
-            // 동명이인이 있을경우 저장된 값을 들고와서 +1 진행
-            map.put(name,map.getOrDefault(name,0)+1);
+            map.put(name, map.getOrDefault(name,0)+1 );
         }
+        //System.out.println(map);
         
         for(String name : completion){
-            map.put(name, map.get(name) - 1);
+            map.put(name,map.get(name) -1);
         }
         
-        for(String name : map.keySet()){
-            if(map.get(name) > 0 ){
-                answer = name;
+        //System.out.println(map);
+        
+        for(int i=0; i<participant.length; i++){
+            if(map.get(participant[i]) > 0 ){
+                answer = participant[i];
             }
         }
+        
         
         return answer;
     }
